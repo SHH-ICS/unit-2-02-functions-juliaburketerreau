f34cf17db4d9b628c19e959c51d5c70a700e95f5
juliaburketerreau <julia.burke-terreau@stu.ocsb.ca>
2024-10-02 14:40:56 +0000

    Tuesdays work

---
+++ functions.py
@@ -0,0 +1,16 @@
+weightlimitmaxa=a
+weightlimitmaxb=b
+weightlimitmaxc=c
+weightlimit=min(weightlimitmaxa, weightlimitmaxb, weightlimitmaxc)
+print(weightlimit)
+
+
+weightlimita=a
+weightlimitb=b
+weightlimitc=c
+weightlimitd=d
+weightlimite=e
+weightlimitf=min(weightlimita,weightlimitb, weightlimitc)
+weightlimitg=min(max(weightlimitd, weightlimitf), weightlimite)
+weightlimith=max(weightlimitf,weightlimitg)
+print(weightlimith)
